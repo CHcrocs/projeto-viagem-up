6673e5c2ca2c8ba5b10e9a389c75aaa1fb886e47
Adriel <adrieltraningdev@gmail.com>
2024-05-14 14:31:22 -0300

    Criaçao de pasta e add de bibliotecas
    criaçao da pasta models e add das bibliotecas "Microsoft.EntityFrameworkCore.Sqlite" e "Microsoft.EntityFrameworkCore.Design"

--- Projeto/Projeto/obj/Debug/net8.0/Projeto.AssemblyInfo.cs
+++ Projeto/Projeto/obj/Debug/net8.0/Projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ace5c6d175ad1bc3ea3c2ed22e85b0b9fd2363")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96daf8b316fb26cb098453fb617963a1a54cdd81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
